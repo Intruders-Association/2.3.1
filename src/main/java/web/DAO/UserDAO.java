bf3e961493b71e706957c9c7f4f669c73421adeb
Aleksandr Bobrov <99945540+723alb@users.noreply.github.com>
2024-03-19 20:56:24 +0300

    Add files via upload

---
+++ src/main/java/web/DAO/UserDAO.java
@@ -0,0 +1,13 @@
+package web.DAO;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDAO {
+    void addUser(User user);
+    void deleteUser(long id);
+    void updateUser(User user);
+    User findUser(long id);
+    List<User> getUserTable();
+}
